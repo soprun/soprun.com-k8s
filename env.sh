5655967aac0270bec4f7561c8580eea46cf38726
Vladislav Soprun <develop@soprun.com>
2020-05-08 11:53:35 +0300

    commit

--- env.sh
+++ env.sh
@@ -3,15 +3,15 @@
 set -ex
 
 # set environment variables: Google Cloud Platform
-export PROJECT_ID="${PROJECT_ID:-project-40825}"
-export PROJECT_NUMBER="${PROJECT_NUMBER:-797048000142}"
+export PROJECT_ID=${PROJECT_ID:-project-40825}
+export PROJECT_NUMBER=${PROJECT_NUMBER:-797048000142}
 export WORKLOAD_IDENTITY="${PROJECT_ID}.svc.id.goog"
 
 # Cluster
-export CLUSTER_NAME="${CLUSTER_NAME:-cluster}"
-export CLUSTER_REGION="${CLUSTER_REGION:-europe-west3}"
-export CLUSTER_ZONE="${CLUSTER_REGION:-europe-west3-a}"
-export CLUSTER_LOCATION="${CLUSTER_LOCATION:-$CLUSTER_ZONE}" # ZONE_OR_REGION
+export CLUSTER_NAME=${CLUSTER_NAME:-cluster}
+export CLUSTER_REGION=${CLUSTER_REGION:-europe-west3}
+export CLUSTER_ZONE=${CLUSTER_REGION:-europe-west3-a}
+export CLUSTER_LOCATION=${CLUSTER_LOCATION:-$CLUSTER_ZONE} # ZONE_OR_REGION
 
 # Configure default
 # gcloud config set project ${PROJECT_ID}
@@ -27,7 +27,7 @@ export NETWORK_EXTERNAL_IP_NAME="static-ip"
 # export NETWORK_EXTERNAL_IPV6=""
 # export NETWORK_EXTERNAL_IPV6_NAME="static-ipv6"
 
-export APP_DOMAIN_NAME="${APP_DOMAIN_NAME:-soprun.com}"
+export APP_DOMAIN_NAME=${APP_DOMAIN_NAME:-soprun.com}
 export APP_INSTANCE_NAME="app"
 export APP_NAMESPACE="publication"
 
