237c5db7c097854c118e9562b23dd4d06d17a24a
Vladislav Soprun <develop@soprun.com>
2020-05-11 22:44:19 +0300

    commit

--- cluster-create.sh
+++ cluster-create.sh
@@ -14,6 +14,7 @@ machine_type="g1-small"
 image_type="COS"
 disk_type="pd-standard"
 disk_size=100
+default_pool_num_nodes=3
 
 gcloud beta container --project ${PROJECT_ID} clusters create ${CLUSTER_NAME} \
   --zone ${CLUSTER_ZONE} \
@@ -25,42 +26,24 @@ gcloud beta container --project ${PROJECT_ID} clusters create ${CLUSTER_NAME} \
   --disk-size ${disk_size} \
   --metadata disable-legacy-endpoints=true \
   --scopes "https://www.googleapis.com/auth/devstorage.read_only","https://www.googleapis.com/auth/logging.write","https://www.googleapis.com/auth/monitoring","https://www.googleapis.com/auth/servicecontrol","https://www.googleapis.com/auth/service.management.readonly","https://www.googleapis.com/auth/trace.append" \
+  --num-nodes ${default_pool_num_nodes} \
   --enable-stackdriver-kubernetes \
   --enable-ip-alias \
-  --cluster-ipv4-cidr=10.0.0.0/21 \
-  --services-ipv4-cidr=10.4.0.0/19 \
-  --create-subnetwork name="cluster-name-subnet",range=/21 \
   --network "projects/project-40825/global/networks/default" \
   --subnetwork "projects/project-40825/regions/europe-west3/subnetworks/default" \
   --enable-intra-node-visibility \
-  --default-max-pods-per-node=28 \
-  --min-nodes "1" \
-  --max-nodes "3" \
-  --max-pods-per-node "28" \
+  --default-max-pods-per-node "110" \
+  --enable-network-policy \
   --no-enable-master-authorized-networks \
-  --addons HorizontalPodAutoscaling,HttpLoadBalancing,GcePersistentDiskCsiDriver \
+  --addons HorizontalPodAutoscaling,HttpLoadBalancing,NodeLocalDNS,GcePersistentDiskCsiDriver \
   --enable-autoupgrade \
-  --enable-autoscaling \
   --enable-autorepair \
   --max-surge-upgrade 1 \
   --max-unavailable-upgrade 0 \
+  --enable-tpu \
   --resource-usage-bigquery-dataset "cluster_usage_metering" \
   --enable-resource-consumption-metering \
   --identity-namespace ${WORKLOAD_IDENTITY} \
   --enable-shielded-nodes \
   --shielded-secure-boot \
   --security-group "gke-security-groups@soprun.com"
-
-# WARNING: The Pod address range limits the maximum size of the cluster.
-# Please refer to https://cloud.google.com/kubernetes-engine/docs/how-to/flexible-pod-cidr
-# to learn how to optimize IP address allocation.
-#
-# This will enable the autorepair feature for nodes.
-# Please see https://cloud.google.com/kubernetes-engine/docs/node-auto-repair
-# for more information on node autorepairs.
-
-
-# Created [https://container.googleapis.com/v1beta1/projects/project-40825/zones/europe-west3-a/clusters/cluster].
-# To inspect the contents of your cluster, go to:
-# https://console.cloud.google.com/kubernetes/workload_/gcloud/europe-west3-a/cluster?project=project-40825
-# kubeconfig entry generated for cluster.
