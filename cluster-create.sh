eef0c8896a3253c354fe6a1cb7cf406f0974c6ba
Vladislav Soprun <develop@soprun.com>
2020-05-12 09:34:56 +0300

    commit: sandbox-cluster-6

--- cluster-create.sh
+++ cluster-create.sh
@@ -14,7 +14,7 @@ tags="default-allow-ssh,default-allow-http,default-allow-https"
 scopes="storage-ro,logging-write,monitoring,service-control,service-management,trace"
 metadata="disable-legacy-endpoints=true"
 
-echo gcloud container clusters create ${GKE_CLUSTER_NAME} --project ${GCP_PROJECT_ID} \
+gcloud beta container clusters create ${GKE_CLUSTER_NAME} --project ${GCP_PROJECT_ID} \
   --user-output-enabled \
   --verbosity=info \
   --zone=${GKE_CLUSTER_LOCATION} \
