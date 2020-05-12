907397b9f346825fd1e5894c4fa73298bde104f1
Vladislav Soprun <develop@soprun.com>
2020-05-12 11:34:39 +0300

    clusters create: cluster-sandbox-12068

--- cluster-create.sh
+++ cluster-create.sh
@@ -18,11 +18,10 @@ ApplicationManager,\
 GcePersistentDiskCsiDriver\
 "
 
-git commit -a -S -m "commit clusters: ${GKE_CLUSTER_NAME}"
+git commit -a -S -m "clusters create: ${GKE_CLUSTER_NAME}"
 
 gcloud beta container clusters create ${GKE_CLUSTER_NAME} --project ${GCP_PROJECT_ID} \
-  --zone=${GKE_CLUSTER_LOCATION} \
-  --log-http \
+  --region europe-west3 \
   --no-enable-basic-auth \
   --release-channel regular \
   --workload-pool ${GKE_WORKLOAD_IDENTITY} \
