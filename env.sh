3d0a22a9a50bd59e1319e51339c60f51cc3f0a2d
Vladislav Soprun <develop@soprun.com>
2020-06-21 03:07:36 +0300

    Deployment cluster

--- env.sh
+++ env.sh
@@ -10,10 +10,10 @@ export CLOUDSQL_ID="shared-database"
 export CLOUDSQL_NAME="${PROJECT_ID}:${CLUSTER_REGION}:${CLOUDSQL_ID}"
 # export CLOUDSQL_ROOT_PASSWORD="oGeqk8NshH6AEavD"
 
-export CLOUDSQL_ENDPOINT="10.98.160.7"
-export CLOUDSQL_USERNAME="sandbox"
-export CLOUDSQL_PASSWORD="113w3KbyJ8LkQOP"
-export CLOUDSQL_DATABASE_NAME="sandbox_database"
+export DATABASE_ENDPOINT="10.98.160.7"
+export DATABASE_USERNAME="sandbox"
+export DATABASE_PASSWORD="113w3KbyJ8LkQOP"
+export DATABASE_DATABASE_NAME="sandbox_database"
 
 # ...
 # ----------------------------------
@@ -21,3 +21,5 @@ export CLOUDSQL_DATABASE_NAME="sandbox_database"
 export DEPLOYMENT_DOMAIN="soprun.com"
 export DEPLOYMENT_NAMESPACE="sandbox"
 export DEPLOYMENT_INSTANCE_NAME="instance"
+
+# projects/project-40825/locations/europe-west3/keyRings/default/cryptoKeys/default-encryption-decrypt
